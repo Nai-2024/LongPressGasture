9ea8c53d80ddfff6280c93fc794aab67a8d4e934
Din <Din.Salehy@triosstudent.com>
2025-03-21 08:12:54 -0400

    Initial Commit

---
+++ LongPressGasture/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LongPressGasture
+//
+//  Created by Din Salehy on 2025-03-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LongPressGasture/LongPressGastureApp.swift
@@ -0,0 +1,17 @@
+//
+//  LongPressGastureApp.swift
+//  LongPressGasture
+//
+//  Created by Din Salehy on 2025-03-21.
+//
+
+import SwiftUI
+
+@main
+struct LongPressGastureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
